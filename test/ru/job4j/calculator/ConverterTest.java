0c7aed8a9816643c9658ec5df8a143eb397e00c5
Crash <info@sampx.ru>
2021-06-30 19:12:50 +0300

    1. Модульные тесты. [#159435 #173073]

--- test/ru/job4j/calculator/ConverterTest.java
+++ test/ru/job4j/calculator/ConverterTest.java
@@ -2,34 +2,31 @@ package ru.job4j.calculator;
 
 import org.junit.Assert;
 import org.junit.Test;
-import ru.job4j.calculator.Converter;
-
 public class ConverterTest {
 
     @Test
     public void whenConvert140RblThen2Euro() {
         float in = 140;
-        float expected = (float) (in / 86.21);
+        float expected = (float) 1.62;
+        System.out.println(expected);
         float out = Converter.rubleToEuro(in);
-        float eps = 0.0001f;
-        Assert.assertEquals(expected, out, eps);
+        Assert.assertEquals(expected, out, 0.01f);
     }
 
     @Test
     public void whenConvert600RblThen8Dlr() {
         float in = 600;
-        float expected = (float) (in / 72.26);
+        float expected = (float) 8.30;
+        System.out.println(expected);
         float out = Converter.rubleToDollar(in);
-        float eps = 0.0001f;
-        Assert.assertEquals(expected, out, eps);
+        Assert.assertEquals(expected, out, 0.01f);
     }
 
     @Test
     public void whenConvert0RblThen0Dlr() {
         float in = 0;
-        float expected = (float) (in / 72.26);
+        float expected = 0;
         float out = Converter.rubleToDollar(in);
-        float eps = 0.0001f;
-        Assert.assertEquals(expected, out, eps);
+        Assert.assertEquals(expected, out, 0.01f);
     }
 }
