77b9e6033780df4562dcf9e832532a5531c8742d
Crash <info@sampx.ru>
2021-07-01 01:09:30 +0300

    2. Тесты для задачи "идеальный вес" [#159458]

---
+++ test/ru/job4j/calculator/FitTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.calculator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FitTest {
+
+    @Test
+    public void whenMan140Then46() {
+        float in = 140;
+        float expected = (float) 46;
+        System.out.println(expected);
+        short out = (short) Fit.manWeight((short) in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenWoman120Then46() {
+        float in = 120;
+        float expected = (float) 23;
+        System.out.println(expected);
+        short out = (short) Fit.womanWeight((short) in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
