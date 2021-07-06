0e683c527bb64edc624f30b8f820b6523673f0f5
Crash <info@sampx.ru>
2021-07-06 20:36:15 +0300

    5.0. Цикл for. Определение. [#3572]

--- src/ru/job4j/condition/ChessBoard.java
+++ src/ru/job4j/condition/ChessBoard.java
@@ -3,7 +3,9 @@ package ru.job4j.condition;
 public class ChessBoard {
     public static int way(int x1, int y1, int x2, int y2) {
         int rls = 0;
-        if (x1 + y1 == x2 + y2) {
+        if (x2 < 0 || x2 > 7 || y2 < 0 || y2 > 7 || x1 < 0 || x1 > 7 || y1 < 0 || y1 > 7) {
+            return rls;
+        } else if (x1 + y2 == x2 + y2) {
             rls = Math.abs(y2 - y1);
         }
         return rls;
