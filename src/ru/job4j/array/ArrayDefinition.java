1dd886aee9e93c218a0547690ca2e180681874eb
Crash <info@sampx.ru>
2021-07-09 00:29:22 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -2,12 +2,17 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] ages = new short[10];
         String[] suNames = new String[100500];
+        String[] names = {"Имя 1", "Имя 2", "Имя 3", "Имя 4"};
+        short[] ages = new short[10];
         float[] prices = new float[40];
 
         System.out.println(ages.length);
         System.out.println(suNames.length);
         System.out.println(prices.length);
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
