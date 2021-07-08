61bc26c10ebdddddd911b6bf01da17d8ec40ea85
Crash <info@sampx.ru>
2021-07-08 23:49:48 +0300

    5.4. Протеиновая диета [#156313 #175270]

--- src/ru/job4j/loop/Fitness.java
+++ src/ru/job4j/loop/Fitness.java
@@ -5,8 +5,8 @@ public class Fitness {
         int month = 0;
 
         while (ivan < nik) {
-            ivan *= month;
-            nik *= month;
+            ivan *= 3;
+            nik *= 2;
             month++;
 
         }
