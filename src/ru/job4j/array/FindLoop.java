343b274a0ce5af44654aa07c5678cc759b095d26
Crash <info@sampx.ru>
2021-07-14 23:33:21 +0300

    6.5. Поиск индекса в диапазоне. [#156318]

--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -1,6 +1,16 @@
 package ru.job4j.array;
 
 public class FindLoop {
+    public static int indexOf(int[] data, int el, int start, int finish) {
+        int temp = 0;
+        for (int i = start; i < finish; i++) {
+            if (data[i] == el) {
+                temp = i;
+            }
+        }
+        return temp;
+    }
+
     public static int indexOf(int[] data, int el) {
         int rst = -1;
         for (int index = 0; index < data.length; index++) {
