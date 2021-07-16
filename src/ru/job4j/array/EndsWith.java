1d988bd53c691972b019a4e4c67cb6d62c18ed58
Crash <info@sampx.ru>
2021-07-17 01:37:27 +0300

    6.4.2. Слово заканчивается на ... [#156317 #177807]

--- src/ru/job4j/array/EndsWith.java
+++ src/ru/job4j/array/EndsWith.java
@@ -2,10 +2,13 @@ package ru.job4j.array;
 
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
-        boolean result = true;
-        if (word[word.length - 1] != post[post.length - 1]) {
-            result = false;
+        boolean result = false;
+        for (int i = 0; i < post.length; i++) {
+            if (word[i] == post[i]) {
+                result = true;
+            }
         }
+
         return result;
     }
 }
