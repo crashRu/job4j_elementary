753c618dd2989d07bdd631d8818f98f55db2cc98
Crash <info@sampx.ru>
2021-07-20 23:22:05 +0300

    6.6.1. Размер элемента двухмерного массива. [#333583 #179532]

--- src/ru/job4j/array/LengthArrayArrays.java
+++ src/ru/job4j/array/LengthArrayArrays.java
@@ -3,9 +3,8 @@ package ru.job4j.array;
 public class LengthArrayArrays {
     public static void main(String[] args) {
         int[][] numbers = {{1}, {1, 2}, {1, 2, 3}, {1, 2, 3, 4}};
-        System.out.println(numbers[0].length);
-        System.out.println(numbers[1].length);
-        System.out.println(numbers[2].length);
-        System.out.println(numbers[3].length);
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i].length);
+        }
     }
 }
